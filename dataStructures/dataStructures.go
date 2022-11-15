ac0fa47a0c336b637c3865ee7fce8ccf93fc4729
Tsawlen <78802953+Tsawlen@users.noreply.github.com>
2022-11-15 13:22:58 +0100

    initalize

---
+++ dataStructures/dataStructures.go
@@ -0,0 +1,13 @@
+package dataStructures
+
+type User struct {
+	Id          int    `json:"id"`
+	City        string `json:"city"`
+	Email       string `json:"email"`
+	First_name  string `json:"first_name"`
+	Name        string `json:"name"`
+	Password    string `json:"password"`
+	Street      string `json:"street"`
+	HouseNumber string `json:"houseNumber"`
+	Username    string `json:"username"`
+}
